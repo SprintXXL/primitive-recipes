47e08c450b2ce21f51e7e94627ef017eccb689f9
SprintXXL <mspmain@pm.me>
2026-06-06 09:02:14 +0200

    init commit

---
+++ src/main/java/com/SprintXXL/primitiverecipes/PrimitiveRecipes.java
@@ -0,0 +1,15 @@
+package com.SprintXXL.primitiverecipes;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import static com.SprintXXL.primitiverecipes.Reference.*;
+
+@Mod(modid = MODID, name = NAME, version = VERSION)
+public class PrimitiveRecipes {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+}
