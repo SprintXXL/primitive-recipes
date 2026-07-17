e351552a9b696677f34c4e73b7169220ce95bb44
SprintXXL <mspmain@pm.me>
2026-07-17 21:13:52 +0200

    Time to retire...

--- src/main/java/com/SprintXXL/primitiverecipes/recipes/crafting/definitions/ModCRIDs.java
+++ src/main/java/com/SprintXXL/primitiverecipes/recipes/crafting/definitions/ModCRIDs.java
@@ -16,56 +16,21 @@ public final class ModCRIDs {
     // UTILITYTOOLS \\
     // ************ \\
 
-    // ASSEMBLY \\
-    public static final String ASSEMBLE_CROWBAR = "assemble_crowbar";
-    public static final String ASSEMBLE_FILE = "assemble_file";
-    public static final String ASSEMBLE_HAMMER = "assemble_hammer";
-    public static final String ASSEMBLE_KNIFE = "assemble_knife";
-    public static final String ASSEMBLE_MORTAR = "assemble_mortar";
-    public static final String ASSEMBLE_SAW = "assemble_saw";
-    public static final String ASSEMBLE_SCREWDRIVER = "assemble_screwdriver";
-    public static final String ASSEMBLE_WRENCH = "assemble_wrench";
-
     // ********* \\
     // TOOLWORKS \\
     // ********* \\
 
     // ASSEMBLY \\
-    public static final String ASSEMBLE_MODULAR_TOOL = "assemble_modular_tool";
-    public static final String ASSEMBLE_MODULAR_PART = "assemble_modular_part";
-    public static final String ASSEMBLE_BLANK_PATTERN = "assemble_blank_pattern";
-    public static final String ASSEMBLE_TOOL_STATION = "assemble_tool_station";
-    public static final String ASSEMBLE_PART_BUILDER = "assemble_part_builder";
-    public static final String ASSEMBLE_STENCIL_TABLE = "assemble_stencil_table";
 
     // CUSTOM \\
-    public static final String REPAIR_MODULAR_TOOL = "repair_modular_tool";
-    public static final String APPLY_MODULAR_TOOL_MODIFIER = "apply_modular_tool_modifier";
-    public static final String REPLACE_MODULAR_TOOL_PART = "replace_modular_tool_part";
-    public static final String CARVE_PATTERN = "carve_pattern";
 
     // ********* \\
     // MATERIALS \\
     // ********* \\
 
     // ASSEMBLY \\
-    public static final String ASSEMBLE_IRON_GEAR = "assemble_iron_gear";
-    public static final String ASSEMBLE_BRONZE_GEAR = "assemble_bronze_gear";
-
-    // PROCESSING \\
-    public static final String HAMMER_IRON_PLATE = "hammer_iron_plate";
-    public static final String HAMMER_BRONZE_PLATE = "hammer_bronze_plate";
-    public static final String FILE_IRON_ROD = "file_iron_rod";
-    public static final String FILE_BRONZE_ROD = "file_bronze_rod";
-    public static final String SAW_IRON_BOLT = "saw_iron_bolt";
-    public static final String SAW_BRONZE_BOLT = "saw_bronze_bolt";
-    public static final String FILE_IRON_SCREW = "file_iron_screw";
-    public static final String FILE_BRONZE_SCREW = "file_bronze_screw";
-    public static final String MORTAR_IRON_DUST = "mortar_iron_dust";
-    public static final String MORTAR_BRONZE_DUST = "mortar_bronze_dust";
 
     // MIXING \\
-    public static final String MIX_BRONZE_DUST = "mix_bronze_dust";
 
     // **** \\
     // ORES \\
